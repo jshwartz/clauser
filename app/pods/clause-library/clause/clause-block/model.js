4e38789ff84fb435216f4e22616acbcab9b91280
Jacob Shwartz <jshwartz@gmail.com>
2016-08-24 14:13:26 -0700

    create clause and clause block models and install mirage

--- app/pods/clause-library/clause/clause-block/model.js
+++ app/pods/clause-library/clause/clause-block/model.js
@@ -1,5 +1,15 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  clause: DS.belongsTo('clause'),
+  type: DS.attr('string'),
+  menu: DS.attr('string'),
+  staticText: DS.attr('string'),
+  helpText: DS.attr('string'),
+  defaultActive: DS.attr('boolean', { defaultValue: false }),
+  toggleOn: DS.attr('boolean', { defaultValue: true }),
+  orderNumber: DS.attr('number'),
+
+
 
 });
--- app/pods/clause-library/clause/model.js
+++ app/pods/clause-library/clause/model.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  clauseBlocks: DS.hasMany('clause-block'),
+  name: DS.attr('string'),
+  title: DS.attr('string'),
+  createdAt: DS.attr('date', {
+    defaultValue() { return new Date(); }
+  }),
 
 });
