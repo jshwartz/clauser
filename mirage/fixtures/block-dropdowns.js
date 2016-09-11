1c24e8578976b550d2e3efd00d31f828a38a4296
Jacob Shwartz <jshwartz@gmail.com>
2016-09-11 11:15:45 -0700

    right before materilize

--- mirage/fixtures/block-dropdowns.js
+++ mirage/fixtures/block-dropdowns.js
@@ -20,4 +20,18 @@ export default [
     defaultTrue: false,
     clauseBlockId: 1
   },
+  {id: 4,
+    orderNumber: 1,
+    menuText: 'Capped at contract price',
+    text: "TO THE MAXIMUM EXTENT PERMITTED BY LAW, IN ANY ACTION FOR DAMAGES ARISING UNDER THIS AGREEMENT, EACH PARTY'S MONETARY DAMAGES WILL BE LIMITED TO THE TOTAL MONETARY AMOUNT RECIEVED UNDER THIS AGREEMENT.",
+    defaultTrue: false,
+    clauseBlockId: 4
+  },
+  {id: 5,
+    orderNumber: 2,
+    menuText: 'Capped at fixed dollar amount',
+    text: "TO THE MAXIMUM EXTENT PERMITTED BY LAW, IN ANY ACTION FOR DAMAGES ARISING UNDER THIS AGREEMENT, EACH PARTY'S MONETARY DAMAGES WILL BE LIMITED TO $_____________.",
+    defaultTrue: true,
+    clauseBlockId: 4
+  },
 ];
--- mirage/fixtures/clause-blocks.js
+++ mirage/fixtures/clause-blocks.js
@@ -13,4 +13,17 @@ export default [
     helpText: 'See documentation for the consequences of catagorically excluding damages. Note that liability caps will be more reliable than this',
     clauseId: 1
   },
+  {id: 3,
+    orderNumber: 3,
+    type: 'static',
+    staticText: ', WHETHER BASED ON BREACH OF CONTRACT, BREACH OF WARRANTY, TORT (INCLUDING NEGLIGENCE, PRODUCT LIABILITY OR OTHERWISE), OR ANY OTHER PECUNIARY LOSS.',
+    clauseId: 1
+  },
+  {id: 4,
+    orderNumber: 4,
+    type: 'dropdown',
+    title: 'Choose damage cap.',
+    helpText: 'See documentation for discussion on damage caps',
+    clauseId: 1
+  },
 ];
